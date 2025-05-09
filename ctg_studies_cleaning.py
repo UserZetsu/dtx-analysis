b5e38715f99ae584a33417cc7bf7b0641054b345
Jeff Lin <jefflinbc@gmail.com>
2025-05-09 12:27:57 -0700

    Updated dataset

---
+++ ctg_studies_cleaning.py
@@ -0,0 +1,71 @@
+import json
+import pandas as pd
+
+def main(): 
+    # Load Json
+    with open('data/ctg-studies.json', 'r') as file:
+        studies = json.load(file)
+        
+    rows = list()
+    for study in studies:
+        # Protocol Data
+        protocol = study.get('protocolSection', {})
+        
+        # Identification Module
+        identification = protocol.get('identificationModule', {})
+        nct_id = identification.get('nctId')
+        org_name = identification.get('organization', {}).get('fullName')
+        org_class = identification.get('organization', {}).get('class')
+        brief_title = identification.get('briefTitle')
+
+        # Status Module
+        status = protocol.get('statusModule', {})
+        overall_status = status.get('overallStatus')
+        why_stopped = status.get('whyStopped')
+
+        # Sponsor/Collaborators Module
+        sponsors = protocol.get('sponsorCollaboratorsModule', {})
+        collaborators = sponsors.get('collaborators')
+
+        # Oversight Module
+        oversight = protocol.get('oversightModule', {})
+        has_dmc = oversight.get('oversightHasDmc')
+        fda_regulated_drug = oversight.get('isFdaRegulatedDrug')
+        fda_regulated_device = oversight.get('isFdaRegulatedDevice')
+
+        # Design Module
+        design = protocol.get('designModule', {})
+        phases = design.get('phases', [])
+        phase = phases[0] if phases else None
+
+        # Conditions Module
+        conditions_module = protocol.get('conditionsModule', {})
+        conditions = conditions_module.get('conditions')
+        keywords = conditions_module.get('keywords')  
+
+        rows.append({
+            'nct_id': nct_id,
+            'org_name': org_name,
+            'org_class': org_class,
+            'brief_title': brief_title,
+            'overall_status': overall_status,
+            'why_stopped': why_stopped,
+            'collaborators': collaborators,
+            'has_dmc': has_dmc,
+            'fda_regulated_drug': fda_regulated_drug,
+            'fda_regulated_device': fda_regulated_device,
+            'phase': phase,
+            'conditions': conditions,
+            'keywords': keywords  
+        })
+
+    # Convert to DataFrame
+    df = pd.DataFrame(rows)
+    
+    return df
+    
+if __name__ == "__main__":
+    df = main()
+    
+    # Save to CSV
+    df.to_csv('data/pulled_ctg_studies.csv', index=False)
